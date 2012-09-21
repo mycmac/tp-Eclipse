2d201f164a4cad810aee0010388d13d01289e733
mycmac <myriam.servieres@ec-nantes.fr>
2012-09-21 10:05:53 +0200

    Test.

---
+++ TP/src/Test.java
@@ -0,0 +1,18 @@
+import geometry.Point;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+		
+		Point p = new Point(10, 20);
+		p.translate(5, 10);
+		System.out.println(p);
+	}
+
+}
